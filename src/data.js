b6c2636f45042bba4adae814900fa2b4afb52592
Fernando Valani Cavalcante <valanife@gmail.com>
2021-06-10 18:22:20 -0300

    create server

---
+++ src/data.js
@@ -0,0 +1,13 @@
+let participants = [{ name: "João", lastStatus: 12313123 }];
+
+let messages = [
+  {
+    from: "João",
+    to: "Todos",
+    text: "oi galera",
+    type: "message",
+    time: "20:04:37",
+  },
+];
+
+export default { participants, messages };
