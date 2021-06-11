5a38df77029aad12717cff92aebc04f50429e91a
Fernando Valani Cavalcante <valanife@gmail.com>
2021-06-11 00:01:20 -0300

    get participants done

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import postParticipants from "./postParticipants.js";
+import participants from "./participants.js";
+import messages from "./messages.js";
 
 const app = express();
 app.use(cors());
@@ -8,6 +10,10 @@ app.use(express.json());
 
 app.post("/participants", (req, res) => {
   postParticipants(req, res);
+});
+
+app.get("/participants", (req, res) => {
+  res.send(participants);
 });
 
 app.listen(4000, () => {
