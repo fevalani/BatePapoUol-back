439908358297351c4a41b1747867b8488d6b68a6
thiagoribb <thiribeiro142@gmail.com>
2021-06-15 17:13:49 -0300

    Preparando review

--- src/getMessages.js
+++
@@ -1,9 +0,0 @@
-export default function getMessages(req, res, participants, messages) {
-  const objMessages = messages.filter(
-    (item) =>
-      item.to === req.headers.user ||
-      item.to === "Todos" ||
-      item.from === req.headers.user
-  );
-  res.send(objMessages.slice(-(req.query.limit || participants.length)));
-}
